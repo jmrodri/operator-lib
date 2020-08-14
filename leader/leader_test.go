cd7f113c2cc718ea535010af49757a7024426066
jesus m. rodriguez <jesusr@redhat.com>
2020-08-14 01:12:20 -0400

    test leader with reactor client

--- leader/leader_test.go
+++ leader/leader_test.go
@@ -16,6 +16,7 @@ package leader
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os"
 
@@ -23,8 +24,10 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/operator-framework/operator-lib/test"
 	corev1 "k8s.io/api/core/v1"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/testing"
 	crclient "sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
@@ -34,8 +37,7 @@ var _ = Describe("Leader election", func() {
 
 	Describe("Become", func() {
 		var (
-			client crclient.Client
-			//	reactor crclient.Client
+			client  crclient.Client
 			reactor test.ReactorClient
 		)
 		BeforeEach(func() {
@@ -69,38 +71,239 @@ var _ = Describe("Leader election", func() {
 			)
 			reactor = test.NewReactorClient(client)
 		})
-		// It("should return an error when POD_NAME is not set", func() {
-		//     os.Unsetenv("POD_NAME")
-		//     err := Become(context.TODO(), "leader-test")
-		//     Expect(err).ShouldNot(BeNil())
-		// })
-		// It("should return an ErrNoNamespace", func() {
-		//     os.Setenv("POD_NAME", "leader-test")
-		//     readNamespace = func() ([]byte, error) {
-		//         return nil, os.ErrNotExist
-		//     }
-		//     err := Become(context.TODO(), "leader-test", WithClient(client))
-		//     Expect(err).ShouldNot(BeNil())
-		//     Expect(err).To(Equal(ErrNoNamespace))
-		//     Expect(errors.Is(err, ErrNoNamespace)).To(Equal(true))
-		// })
+		It("should return an error when POD_NAME is not set", func() {
+			os.Unsetenv("POD_NAME")
+			err := Become(context.TODO(), "leader-test")
+			Expect(err).ShouldNot(BeNil())
+		})
+		It("should return an ErrNoNamespace", func() {
+			os.Setenv("POD_NAME", "leader-test")
+			readNamespace = func() ([]byte, error) {
+				return nil, os.ErrNotExist
+			}
+			err := Become(context.TODO(), "leader-test", WithClient(client))
+			Expect(err).ShouldNot(BeNil())
+			Expect(err).To(Equal(ErrNoNamespace))
+			Expect(errors.Is(err, ErrNoNamespace)).To(Equal(true))
+		})
 		It("should not return an error", func() {
 			os.Setenv("POD_NAME", "leader-test")
 			readNamespace = func() ([]byte, error) {
 				return []byte("testns"), nil
 			}
-			//reactor.(*testing.Fake).PrependReactor("create", "configmap", func(action testing.Action) (bool, runtime.Object, error) {
-			reactor.PrependReactor("create", "configmap", func(action testing.Action) (bool, runtime.Object, error) {
-				return true, &corev1.ConfigMap{}, fmt.Errorf("REACTOR CALLED")
 
-			})
-			fmt.Printf("reactionchain length: %v\n", len(reactor.ReactionChain))
-			if len(reactor.ReactionChain) > 0 {
-				fmt.Printf("reactionchain: %v\n", reactor.ReactionChain[0])
+			err := Become(context.TODO(), "leader-test", WithClient(client))
+			Expect(err).Should(BeNil())
+		})
+		It("should return Unknown error trying to get ConfigMap", func() {
+			os.Setenv("POD_NAME", "leader-test")
+			readNamespace = func() ([]byte, error) {
+				return []byte("testns"), nil
+			}
+			reactor.PrependReactor("get", "configmaps",
+				func(action testing.Action) (bool, runtime.Object, error) {
+					return true, &corev1.ConfigMap{}, fmt.Errorf("random error")
+				})
+			err := Become(context.TODO(), "leader-test", WithClient(reactor))
+			Expect(err).ShouldNot(BeNil())
+			Expect(err.Error()).To(Equal("random error"))
+		})
+		It("should return Unknown error trying to create ConfigMap lock", func() {
+			os.Setenv("POD_NAME", "leader-test")
+			readNamespace = func() ([]byte, error) {
+				return []byte("testns"), nil
+			}
+			reactor.PrependReactor("get", "configmaps",
+				func(action testing.Action) (bool, runtime.Object, error) {
+					return true, &corev1.ConfigMap{}, apierrors.NewNotFound(
+						schema.GroupResource{Group: "", Resource: "configmaps"},
+						"reactor")
+				})
+			reactor.PrependReactor("create", "configmaps",
+				func(action testing.Action) (bool, runtime.Object, error) {
+					return true, &corev1.ConfigMap{}, fmt.Errorf("random error")
+				})
+			err := Become(context.TODO(), "leader-test", WithClient(reactor))
+			Expect(err).ShouldNot(BeNil())
+			Expect(err.Error()).To(Equal("random error"))
+		})
+		It("should handle when ConfigMap already exists and become leader", func() {
+			os.Setenv("POD_NAME", "leader-test")
+			readNamespace = func() ([]byte, error) {
+				return []byte("testns"), nil
 			}
 
+			getcount := 1
+			createcount := 1
+			reactor.PrependReactor("get", "configmaps",
+				func(action testing.Action) (bool, runtime.Object, error) {
+					getcount -= 1
+					if getcount < 0 {
+						cm := &corev1.ConfigMap{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "leader-test",
+								Namespace: "testns",
+								OwnerReferences: []metav1.OwnerReference{
+									{
+										APIVersion: "v1",
+										Kind:       "Pod",
+										Name:       "leader-test",
+									},
+								},
+							},
+						}
+						return true, cm, nil
+					}
+					return true, &corev1.ConfigMap{}, apierrors.NewNotFound(
+						schema.GroupResource{Group: "", Resource: "configmaps"},
+						"reactor")
+				})
+			reactor.PrependReactor("create", "configmaps",
+				func(action testing.Action) (bool, runtime.Object, error) {
+					createcount -= 1
+					if createcount < 0 {
+						cm := &corev1.ConfigMap{
+							ObjectMeta: metav1.ObjectMeta{
+								Name:      "leader-test",
+								Namespace: "testns",
+								OwnerReferences: []metav1.OwnerReference{
+									{
+										APIVersion: "v1",
+										Kind:       "Pod",
+										Name:       "leader-test",
+									},
+								},
+							},
+						}
+						return true, cm, nil
+					}
+					return true, &corev1.ConfigMap{}, apierrors.NewAlreadyExists(
+						schema.GroupResource{Group: "", Resource: "configmaps"},
+						"reactor")
+				})
 			err := Become(context.TODO(), "leader-test", WithClient(reactor))
+			Expect(err).ShouldNot(BeNil())
+		})
+	})
+	Describe("isPodEvicted", func() {
+		var (
+			leaderPod *corev1.Pod
+		)
+		BeforeEach(func() {
+			leaderPod = &corev1.Pod{}
+		})
+		It("should return false with an empty status", func() {
+			Expect(isPodEvicted(*leaderPod)).To(Equal(false))
+		})
+		It("should return false if reason is incorrect", func() {
+			leaderPod.Status.Phase = corev1.PodFailed
+			leaderPod.Status.Reason = "invalid"
+			Expect(isPodEvicted(*leaderPod)).To(Equal(false))
+		})
+		It("should return false if pod is in the wrong phase", func() {
+			leaderPod.Status.Phase = corev1.PodRunning
+			Expect(isPodEvicted(*leaderPod)).To(Equal(false))
+		})
+		It("should return true when Phase and Reason are set", func() {
+			leaderPod.Status.Phase = corev1.PodFailed
+			leaderPod.Status.Reason = "Evicted"
+			Expect(isPodEvicted(*leaderPod)).To(Equal(true))
+		})
+	})
+	Describe("getOperatorNamespace", func() {
+		It("should return error when namespace not found", func() {
+			readNamespace = func() ([]byte, error) {
+				return nil, os.ErrNotExist
+			}
+			namespace, err := getOperatorNamespace()
+			Expect(err).To(Equal(ErrNoNamespace))
+			Expect(namespace).To(Equal(""))
+		})
+		It("should return namespace", func() {
+			readNamespace = func() ([]byte, error) {
+				return []byte("testnamespace"), nil
+			}
+
+			// test
+			namespace, err := getOperatorNamespace()
 			Expect(err).Should(BeNil())
+			Expect(namespace).To(Equal("testnamespace"))
+		})
+		It("should trim whitespace from namespace", func() {
+			readNamespace = func() ([]byte, error) {
+				return []byte("   testnamespace    "), nil
+			}
+
+			// test
+			namespace, err := getOperatorNamespace()
+			Expect(err).Should(BeNil())
+			Expect(namespace).To(Equal("testnamespace"))
+		})
+	})
+	Describe("myOwnerRef", func() {
+		var (
+			client crclient.Client
+		)
+		BeforeEach(func() {
+			client = fake.NewFakeClient(
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "mypod",
+						Namespace: "testns",
+					},
+				},
+			)
+		})
+		It("should return an error when POD_NAME is not set", func() {
+			os.Unsetenv("POD_NAME")
+			_, err := myOwnerRef(context.TODO(), client, "")
+			Expect(err).ShouldNot(BeNil())
+		})
+		It("should return an error if no pod is found", func() {
+			os.Setenv("POD_NAME", "thisisnotthepodyourelookingfor")
+			_, err := myOwnerRef(context.TODO(), client, "")
+			Expect(err).ShouldNot(BeNil())
+		})
+		It("should return the owner reference without error", func() {
+			os.Setenv("POD_NAME", "mypod")
+			owner, err := myOwnerRef(context.TODO(), client, "testns")
+			Expect(err).Should(BeNil())
+			Expect(owner.APIVersion).To(Equal("v1"))
+			Expect(owner.Kind).To(Equal("Pod"))
+			Expect(owner.Name).To(Equal("mypod"))
+		})
+	})
+	Describe("getPod", func() {
+		var (
+			client crclient.Client
+		)
+		BeforeEach(func() {
+			client = fake.NewFakeClient(
+				&corev1.Pod{
+					ObjectMeta: metav1.ObjectMeta{
+						Name:      "mypod",
+						Namespace: "testns",
+					},
+				},
+			)
+		})
+		It("should return an error when POD_NAME is not set", func() {
+			os.Unsetenv("POD_NAME")
+			_, err := getPod(context.TODO(), nil, "")
+			Expect(err).ShouldNot(BeNil())
+		})
+		It("should return an error if no pod is found", func() {
+			os.Setenv("POD_NAME", "thisisnotthepodyourelookingfor")
+			_, err := getPod(context.TODO(), client, "")
+			Expect(err).ShouldNot(BeNil())
+		})
+		It("should return the pod with the given name", func() {
+			os.Setenv("POD_NAME", "mypod")
+			pod, err := getPod(context.TODO(), client, "testns")
+			Expect(err).Should(BeNil())
+			Expect(pod).ShouldNot(BeNil())
+			Expect(pod.TypeMeta.APIVersion).To(Equal("v1"))
+			Expect(pod.TypeMeta.Kind).To(Equal("Pod"))
 		})
 	})
 
@@ -242,125 +445,4 @@ var _ = Describe("Leader election", func() {
 		})
 
 	})
-	// Describe("isPodEvicted", func() {
-	//     var (
-	//         leaderPod *corev1.Pod
-	//     )
-	//     BeforeEach(func() {
-	//         leaderPod = &corev1.Pod{}
-	//     })
-	//     It("should return false with an empty status", func() {
-	//         Expect(isPodEvicted(*leaderPod)).To(Equal(false))
-	//     })
-	//     It("should return false if reason is incorrect", func() {
-	//         leaderPod.Status.Phase = corev1.PodFailed
-	//         leaderPod.Status.Reason = "invalid"
-	//         Expect(isPodEvicted(*leaderPod)).To(Equal(false))
-	//     })
-	//     It("should return false if pod is in the wrong phase", func() {
-	//         leaderPod.Status.Phase = corev1.PodRunning
-	//         Expect(isPodEvicted(*leaderPod)).To(Equal(false))
-	//     })
-	//     It("should return true when Phase and Reason are set", func() {
-	//         leaderPod.Status.Phase = corev1.PodFailed
-	//         leaderPod.Status.Reason = "Evicted"
-	//         Expect(isPodEvicted(*leaderPod)).To(Equal(true))
-	//     })
-	// })
-	// Describe("getOperatorNamespace", func() {
-	//     It("should return error when namespace not found", func() {
-	//         readNamespace = func() ([]byte, error) {
-	//             return nil, os.ErrNotExist
-	//         }
-	//         namespace, err := getOperatorNamespace()
-	//         Expect(err).To(Equal(ErrNoNamespace))
-	//         Expect(namespace).To(Equal(""))
-	//     })
-	//     It("should return namespace", func() {
-	//         readNamespace = func() ([]byte, error) {
-	//             return []byte("testnamespace"), nil
-	//         }
-	//
-	//         // test
-	//         namespace, err := getOperatorNamespace()
-	//         Expect(err).Should(BeNil())
-	//         Expect(namespace).To(Equal("testnamespace"))
-	//     })
-	//     It("should trim whitespace from namespace", func() {
-	//         readNamespace = func() ([]byte, error) {
-	//             return []byte("   testnamespace    "), nil
-	//         }
-	//
-	//         // test
-	//         namespace, err := getOperatorNamespace()
-	//         Expect(err).Should(BeNil())
-	//         Expect(namespace).To(Equal("testnamespace"))
-	//     })
-	// })
-	// Describe("myOwnerRef", func() {
-	//     var (
-	//         client crclient.Client
-	//     )
-	//     BeforeEach(func() {
-	//         client = fake.NewFakeClient(
-	//             &corev1.Pod{
-	//                 ObjectMeta: metav1.ObjectMeta{
-	//                     Name:      "mypod",
-	//                     Namespace: "testns",
-	//                 },
-	//             },
-	//         )
-	//     })
-	//     It("should return an error when POD_NAME is not set", func() {
-	//         os.Unsetenv("POD_NAME")
-	//         _, err := myOwnerRef(context.TODO(), client, "")
-	//         Expect(err).ShouldNot(BeNil())
-	//     })
-	//     It("should return an error if no pod is found", func() {
-	//         os.Setenv("POD_NAME", "thisisnotthepodyourelookingfor")
-	//         _, err := myOwnerRef(context.TODO(), client, "")
-	//         Expect(err).ShouldNot(BeNil())
-	//     })
-	//     It("should return the owner reference without error", func() {
-	//         os.Setenv("POD_NAME", "mypod")
-	//         owner, err := myOwnerRef(context.TODO(), client, "testns")
-	//         Expect(err).Should(BeNil())
-	//         Expect(owner.APIVersion).To(Equal("v1"))
-	//         Expect(owner.Kind).To(Equal("Pod"))
-	//         Expect(owner.Name).To(Equal("mypod"))
-	//     })
-	// })
-	// Describe("getPod", func() {
-	//     var (
-	//         client crclient.Client
-	//     )
-	//     BeforeEach(func() {
-	//         client = fake.NewFakeClient(
-	//             &corev1.Pod{
-	//                 ObjectMeta: metav1.ObjectMeta{
-	//                     Name:      "mypod",
-	//                     Namespace: "testns",
-	//                 },
-	//             },
-	//         )
-	//     })
-	//     It("should return an error when POD_NAME is not set", func() {
-	//         os.Unsetenv("POD_NAME")
-	//         _, err := getPod(context.TODO(), nil, "")
-	//         Expect(err).ShouldNot(BeNil())
-	//     })
-	//     It("should return an error if no pod is found", func() {
-	//         os.Setenv("POD_NAME", "thisisnotthepodyourelookingfor")
-	//         _, err := getPod(context.TODO(), client, "")
-	//         Expect(err).ShouldNot(BeNil())
-	//     })
-	//     It("should return the pod with the given name", func() {
-	//         os.Setenv("POD_NAME", "mypod")
-	//         pod, err := getPod(context.TODO(), client, "testns")
-	//         Expect(err).Should(BeNil())
-	//         Expect(pod).ShouldNot(BeNil())
-	//         Expect(pod.TypeMeta.APIVersion).To(Equal("v1"))
-	//         Expect(pod.TypeMeta.Kind).To(Equal("Pod"))
-	//     })
-	// })
 })
